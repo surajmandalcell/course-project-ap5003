85123c5d7ac90093fe305123c56604b0ecf50674
Suraj Mandal <surajmandalcell@gmail.com>
2023-04-04 19:49:01 -0400

    Snapshot before big changes

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
 
 
@@ -7,13 +6,7 @@ def login_user(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            messages.success(request, f"Welcome back, {username}!")
-            return redirect("home")
-        else:
-            messages.error(request, "Invalid username or password.")
+        # Login with firebase username and password
     return render(request, "auth/login.html")
 
 
@@ -21,11 +14,4 @@ def sign_up(request):
     if request.method == "POST":
         username = request.POST["username"]
         password = request.POST["password"]
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            messages.success(request, f"Welcome, {username}!")
-            return redirect("home")
-        else:
-            messages.error(request, "Invalid username or password.")
     return render(request, "auth/signup.html")
