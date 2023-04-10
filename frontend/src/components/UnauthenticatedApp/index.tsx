ebe73945f5514661726e994c69de1301908cdc07
Suraj Mandal <surajmandalcell@gmail.com>
2023-04-10 10:13:34 -0400

    Fix stuff

--- frontend/src/components/UnauthenticatedApp/index.tsx
+++ frontend/src/components/UnauthenticatedApp/index.tsx
@@ -6,8 +6,8 @@ function UnauthenticatedApp() {
   const { login } = useAuth();
 
   return (
-    <div className="flex items-center justify-center w-full h-full">
-      <div className="flex flex-col items-center ">
+    <div className="flex items-center justify-center w-full" style={{ height: '100vh' }}>
+      <div className="flex flex-col items-center justify-center">
         <h2 className="mb-8 text-2xl text-white">
           Log in to join a chat room!
         </h2>
